59cff369ebe7ffc83db31c45025f88db3400a339
Yerassyl <157903613+KarbozErasyl@users.noreply.github.com>
2026-02-22 20:57:08 +0500

    Update dynamic array operations in print.py
    Modified the dynamic array by updating an element, appending a new element, inserting an element at a specific position, and removing an element. Added comments to explain the list methods used.

--- print.py
+++ print.py
@@ -1,6 +1,16 @@
+# dynamic array
 number = [5,8,7,4,6,9,3,2,1]
 print(number[0])
 print(number[1])
 print(number[2])
 print(number[3])
 print(number[4])
+number[2] = 10
+print(number[2])
+number.append(4)
+# append() method is used to add an element at the end of the list
+number.insert(3, 11)
+# insert() method is used to add an element at the specified position in the list
+number.pop(1)
+# pop() method is used to remove an element at the specified position in the list and return it. If no index is specified, it removes and returns the last item in the list.
+print(number)
